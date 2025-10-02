991f4673d3c0f5de8f2f9bdf0bc29e064f383d09
newrizwankhan <patnul.rizwankhan@gmail.com>
2025-10-02 16:23:21 +0530

    code has been updated and other

--- 9.route53.tf
+++ 9.route53.tf
@@ -1,7 +1,7 @@
 module "alpha_route53_1_nlb" {
+  source  = "app.terraform.io/Khans-shop/route53/route53"
+  version = "1.0.0" # or whichever version matched your inputs
 
-  source     = "app.terraform.io/Khans-shop/route53/route53"
-  version    = "1.2.3"
   domainname = var.domainname
   nlb_id     = module.alpha_elb_1.elb_id
   dns_name   = module.alpha_elb_1.elb_dns_name
@@ -9,12 +9,13 @@ module "alpha_route53_1_nlb" {
   recordname = "nlb"
 }
 
-module "alpha_route53_1_alb" {
-  source     = "app.terraform.io/Khans-shop/route53/route53"
-  version    = "1.2.3"
-  domainname = var.domainname
-  nlb_id     = module.alpha_elb_alb_1.elb_id
-  dns_name   = module.alpha_elb_alb_1.elb_dns_name
-  zone_id    = module.alpha_elb_alb_1.elb_zone_id
-  recordname = "alb"
-}
+
+# module "alpha_route53_1_alb" {
+#   source     = "app.terraform.io/Khans-shop/route53/route53"
+#   version    = "1.2.3"
+#   domainname = var.domainname
+#   nlb_id     = module.alpha_elb_alb_1.elb_id
+#   dns_name   = module.alpha_elb_alb_1.elb_dns_name
+#   zone_id    = module.alpha_elb_alb_1.elb_zone_id
+#   recordname = "alb"
+# }
