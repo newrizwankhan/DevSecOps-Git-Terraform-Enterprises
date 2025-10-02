dc524c5a7feed325bd8a45d9644e1b893233cf82
newrizwankhan <patnul.rizwankhan@gmail.com>
2025-10-02 16:15:43 +0530

    code has been updated and other

--- 9.route53.tf
+++ 9.route53.tf
@@ -1,12 +1,12 @@
 module "alpha_route53_1_nlb" {
-  
+
   source     = "app.terraform.io/Khans-shop/route53/route53"
   version    = "1.2.3"
   domainname = var.domainname
   nlb_id     = module.alpha_elb_1.elb_id
   dns_name   = module.alpha_elb_1.elb_dns_name
   zone_id    = module.alpha_elb_1.elb_zone_id
-  recordname = "nlb"
+  # recordname = "nlb"
 }
 
 module "alpha_route53_1_alb" {
@@ -16,5 +16,5 @@ module "alpha_route53_1_alb" {
   nlb_id     = module.alpha_elb_alb_1.elb_id
   dns_name   = module.alpha_elb_alb_1.elb_dns_name
   zone_id    = module.alpha_elb_alb_1.elb_zone_id
-  recordname = "alb"
+  # recordname = "alb"
 }
