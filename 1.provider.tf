564f8897d3ab728190fdb986998cb377f781c67a
newrizwankhan <patnul.rizwankhan@gmail.com>
2025-10-02 13:16:56 +0530

    code has been updated

--- 1.provider.tf
+++ 1.provider.tf
@@ -8,12 +8,12 @@ terraform {
   required_version = "<= 1.12.2" #Forcing which version of Terraform needs to be used
   required_providers {
     aws = {
-      version = "<= 6.0" #Forcing which version of plugin needs to be used.
+      version = "<= 1.12.2" #Forcing which version of plugin needs to be used.
       source  = "hashicorp/aws"
     }
   }
 
-  
+
 #   backend "s3" {
 #     bucket         = "devsecopsawstfstate"
 #     key            = "devsecopsawstfstate-modules.tfstate"
